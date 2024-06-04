28aa9959c4ed90d1ab2909575d8dec5e9fc0f800
Mike Jennings <mike.jennings@imm.ox.ac.uk>
2024-06-04 13:26:56 +0100

    Adapt ChIP pipeline for SRA file names downloaded

--- SRA_download_2.sh
+++ SRA_download_2.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-#SBATCH --partition=long
+#SBATCH --partition=short
 #SBATCH --job-name=SRA
 #SBATCH --ntasks=4
 #SBATCH --mem=120G
-#SBATCH --time=06-23:59:59
+#SBATCH --time=00-23:59:59
 
 module load sratoolkit
 vdb-config --interactive
 
 fastq-dump --split-files SRR15970891 ; #ter119_APH_spleen_rep1
 
-mv SRR15970891_1.fastq SRR15970891_ter119_APH_spleen_rep1_R1.fastq ;
-mv SRR15970891_2.fastq SRR15970891_ter119_APH_spleen_rep1_R2.fastq ;
+mv SRR15970891_1.fastq SRR15970891_ter119_APH_spleen_rep1_R1.fastq ; SRR15970891_2.fastq SRR15970891_ter119_APH_spleen_rep1_R2.fastq ;
 for file in *.fastq ; do
     echo "G-zipping $file" ;
     gzip $file ;
