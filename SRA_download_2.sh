96ed6428ff94d1691a548c331b03b7db9c53d26d
Mike Jennings <mike.jennings@imm.ox.ac.uk>
2024-06-04 11:27:03 +0100

    Derive fastq|-dump script for SRR15970891.

---
+++ SRA_download_2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --partition=long
+#SBATCH --job-name=SRA
+#SBATCH --ntasks=4
+#SBATCH --mem=120G
+#SBATCH --time=06-23:59:59
+
+module load sratoolkit
+
+fastq-dump --split-files SRR15970891 ; #ter119_APH_spleen_rep1
+
+mv SRR15970891_1.fastq SRR15970891_ter119_APH_spleen_rep1_R1.fastq ;
+mv SRR15970891_2.fastq SRR15970891_ter119_APH_spleen_rep1_R2.fastq ;
+for file in *.fastq ; do
+    echo "G-zipping $file" ;
+    gzip $file ;
+done
+
+
+for file in *.fastq.gz ; do echo "$file" ; done
