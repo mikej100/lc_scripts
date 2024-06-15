2d255fd7e48a63d010b53085ae0450a6ae0417ec
Mike Jennings <mike.jennings@imm.ox.ac.uk>
2024-06-15 21:57:00 +0100

    Start change to git reporting.

--- scripts_info.sh
+++ scripts_info.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+remote=$(git -C $(dirname ${BASH_SOURCE}) remote -v | grep -m 1 -o 'http[^ ]*')
+branch=$(git -C $(dirname ${BASH_SOURCE}) rev-parse --abbrev-ref HEAD)
+hash=$(git -C $(dirname ${BASH_SOURCE}) show -s --format=%H)
+info=$(git -C $(dirname ${BASH_SOURCE}) show -s --format=" Date: %ci. Author: %an. %nTitle: %s")
+
 echo
 echo "Script version control: latest commit information"
-remote=$(git -C $(dirname ${BASH_SOURCE}) remote -v | grep -m 1 -o 'http.*\s')
-branch=$(git -C $(dirname ${BASH_SOURCE}) rev-parse --abbrev-ref HEAD)
-echo "github repository: ${remote} Branch: ${branch}"
-git -C $(dirname ${BASH_SOURCE}) show -s --format="Short hash: %h. Date: %ci. Author: %an. %nTitle: %s"
+echo "github repository: ${remote}/tree/${hash}"
+echo "Branch: $branch $info"
 echo
