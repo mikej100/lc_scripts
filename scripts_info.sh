1864e5c0fb707016a162f283c42671a2a89b5e11
Mike Jennings <mike.jennings@imm.ox.ac.uk>
2024-06-15 23:17:16 +0100

    Write github link to log.

--- scripts_info.sh
+++ scripts_info.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-remote=$(git -C $(dirname ${BASH_SOURCE}) remote -v | grep -m 1 -o 'http[^ ]*')
+remote=$(git -C $(dirname ${BASH_SOURCE}) remote -v | grep -m 1 -o '.*' )
+link_stem=$(echo ${remote} | sed -r 's/origin (http.*)\.git \(fetch\)/\1/' )
+
 branch=$(git -C $(dirname ${BASH_SOURCE}) rev-parse --abbrev-ref HEAD)
 hash=$(git -C $(dirname ${BASH_SOURCE}) show -s --format=%H)
 info=$(git -C $(dirname ${BASH_SOURCE}) show -s --format=" Date: %ci. Author: %an. %nTitle: %s")
 
 echo
 echo "Script version control: latest commit information"
-echo "github repository: ${remote}/tree/${hash}"
+echo "github link: ${link_stem}/tree/${hash}"
 echo "Branch: $branch $info"
 echo
