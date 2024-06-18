c2a51b56f34645c4cd6cf41f3598e1886f8a66e9
Mike Jennings <mike.jennings@imm.ox.ac.uk>
2024-06-18 20:08:32 +0100

    Cosmetic code tidy.

--- Step4_P_MultiBigwigSummary_dev.sh
+++ Step4_P_MultiBigwigSummary_dev.sh
@@ -4,36 +4,27 @@
 #SBATCH --error=slurm/%j_%x.out
 #SBATCH --export=SCRIPTS
 #SBATCH --export=TEST_VAR="test value"
-echo "Test environment variable value: ${TEST_VAR}"
+
 # Instructions to run
 # Create directory for he analysis at the same level as the models
 # cd to the analysis directory and run this script
-
-module load python-cbrg/current
-source /project/higgslab/lcornell/mamba_installation/conda/bin/activate bed_sam_deep_tools
-
-
+#=== Boiler plate for job logging ==============================================
 now() {
     date +"%Y-%m-%dT%T"
 }
-NOW=`date +"%Y-%m-%dT%T"`
-
 echo $(now) Starting $(basename "${BASH_SOURCE}")
-workingdir="$(pwd)"
-
 # Show git info for scripts folder
-echo "SCRIPT environment variable set as : $SCRIPTS"
 ${SCRIPTS}/scripts_info.sh || true
+#===============================================================================
+
+#==================== Configuration section ====================================
 # Get the list of models
 
 # Get the models to include in the analysis
 # TODO if file not found then use the models.cfg in the project directory
 mapfile -t model < models.cfg
-
-echo "Starting multiBigwigSummary"
-
 models=${model[@]}
-echo "Models: ${models}"
+echo "Models: ${model[@]}"
 
 bw_filenames=()
 for i in ${!model[@]}; do
@@ -43,13 +34,13 @@ echo "Bigwig filenames: ${bw_filenames}"
 
 bed_filename="../${model[0]}/Lanceotron_output/${model[0]}_L-tron_toppeaks.bed"
 echo "bed filename ${bed_filename}"
-
-
-echo "$(now) Starting multiBigwigSummary"
  
- multiBigwigSummary BED-file \
+#===============================================================================
+echo "$(now) Starting multiBigwigSummary"
+multiBigwigSummary BED-file \
      -b $bw_filenames \
-     -o mbw_results.npq \
+     -o mbw_results.npz \
+     --outRawCounts mbw_results.tab \
      -l $models \
      --BED $bed_filename
 # 
