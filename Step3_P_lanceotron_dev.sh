9fda40f4f451a15a0de324760e3cfa18d89a466b
Mike Jennings <mike.jennings@imm.ox.ac.uk>
2024-07-02 23:58:49 +0100

    Fix Step3 Lanceotron processing.

--- Step3_P_lanceotron_dev.sh
+++ Step3_P_lanceotron_dev.sh
@@ -4,7 +4,7 @@
 #SBATCH --mem=50G
 #SBATCH --time=00-10:00:00
 #SBATCH --output=slurm/%j_%x.out
-#SBATCH --error=slurm/j_%x.out
+#SBATCH --error=slurm/%j_%x.out
 ###SBATCH --mail-type=end,fail
 
 chr_sizes_mm9="/databank/igenomes/Mus_musculus/UCSC/mm9/Sequence/WholeGenomeFasta/chr_sizes.txt"
@@ -47,20 +47,34 @@ model=$(echo $(pwd) | awk -F/ '{print $NF}') ;
 ## Generates a Bigwig of bin size 1 and RPKM normalised, the preferred input for Lanceotron
 #bamCoverage --bam "$bam" -o "$model".bw --extendReads -bs 1 --normalizeUsing RPKM ;
 
-input_file="${model}.bw"
-echo "$(now) Starting Lanceotron for ${input_file}" ;
+bw_file="${model}.bw"
+echo "$(now) Starting Lanceotron for ${bw_file}" ;
 
 # Runs Lanceotron
-lanceotron callPeaks $input_file -f Lanceotron_output
+lanceotron callPeaks $bw_file -f Lanceotron_output
 
-# This filters peak calls to only those of a score greater than 0.8
-echo "Taking top scoring peaks" ;
 
-awk 'BEGIN{FS=OFS="\t"} { if(($4>=0.8) && ($4<=1)) { print $1,$2,$3,$4,$5,$6,$7,$8 } }' Lanceotron_output/"$model"_L-tron.bed > Lanceotron_output/"$model"_L-tron_toppeaks.bed ;
+
+
+L_tron_bed_fname="Lanceotron_output/${model}_L-tron.bed" 
+L_tron_toppeaks_fname="Lanceotron_output/${model}_L-tron_toppeaks.bed"
+
+awk 'BEGIN{FS=OFS="\t"} { if(($4>=0.8) && ($4<=1)) { print $1,$2,$3} }' \
+ $L_tron_bed_fname > \
+ $L_tron_toppeaks_fname
 
 ### Generating a bigbed for UCSC
+L_tron_toppeaks_sorted_fname="Lanceotron_output/${model}_L-tron_toppeaks.sorted.bed"
 module load ucsctools/385
 
-bedToBigBed Lanceotron_output/"$model"_L-tron_toppeaks.bed \
+echo "$(now) Starting sort for ${L_tron_toppeaks_fname}" ;
+sort -k1,1 -k2,2n \
+	$L_tron_toppeaks_fname > \
+	$L_tron_toppeaks_sorted_fname
+	
+L_tron_toppeaks_bb_fname="Lanceotron_output/${model}_L-tron_toppeaks.bb"
+echo "$(now) Starting bedToBigBed for ${L_tron_toppeaks_sorted_fname}"
+bedToBigBed \
+	$L_tron_toppeaks_sorted_fname \
  	$chr_sizes \
-	Lanceotron_output/"$model"_L-tron_toppeaks_bigGenePred.bb ;
+	$L_tron_toppeaks_bb_fname
